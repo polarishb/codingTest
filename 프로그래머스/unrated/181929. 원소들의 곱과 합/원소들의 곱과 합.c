415e1a171fd06adf58639fcdfbc7e085fc346bd6
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-13 21:12:13 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    int a = 1;
+    int b = 0;
+    for ( int i = 0; i < num_list_len; i++){
+        a *= num_list[i];
+        b += num_list[i];
+    }
+    answer = a < (int)pow(b, 2) ? 1 : 0;
+    return answer;
+}
