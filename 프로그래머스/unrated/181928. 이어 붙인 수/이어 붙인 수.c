dbd93ac982072e4570d5f69340e18aaf75d88835
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-13 23:02:41 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.c
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.c
@@ -5,26 +5,16 @@
 // num_list_len은 배열 num_list의 길이입니다.
 int solution(int num_list[], size_t num_list_len) {
     int answer = 0;
-    int ai = 0;
-    int bi = 0;
-    char* a = (char*)malloc(sizeof(char) * num_list_len + 1);
-    char* b = (char*)malloc(sizeof(char) * num_list_len + 1);
-    
-    for(int i = 0; i < num_list_len; i++){
-        a[i] = '\0';
-        b[i] = '\0';
-    }
+    int a = 0;
+    int b = 0;
     
     for(int i = 0; i < num_list_len; i++){
         if(num_list[i] % 2 == 0){
-            a[ai] = num_list[i] + 48;
-            ai++;
+            a = a * 10 + num_list[i];            
         }
-        else{
-            b[bi] = num_list[i] + 48;
-            bi++;
-        }
+        else
+            b = b * 10 + num_list[i];
     }
     
-    return answer = atoi(a) + atoi(b);
+    return answer = a + b;
 }
