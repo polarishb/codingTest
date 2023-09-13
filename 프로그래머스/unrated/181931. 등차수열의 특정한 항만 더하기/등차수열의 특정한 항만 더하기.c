640644b047a2648033cd2715e80a64c674809f49
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-13 20:45:00 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.c
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.c
@@ -5,13 +5,9 @@
 // included_len은 배열 included의 길이입니다.
 int solution(int a, int d, bool included[], size_t included_len) {
     int answer = 0;
-    int sum = a;
     
-    answer = included[0] ? a : 0;
-    
-    for(int i = 1; i < included_len; i++){
-        sum += d;
-        if ( included[i] ) answer += sum;
+    for(int i = 0; i < included_len; i++){
+        if ( included[i] ) answer += a + d * i;
     }
     return answer;
 }
