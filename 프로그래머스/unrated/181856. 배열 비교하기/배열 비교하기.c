42ac6fa7ec0ef519be20144e9cf63ded552da9e0
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-17 15:53:00 +0900

    [unrated] Title: 배열 비교하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr1_len은 배열 arr1의 길이입니다.
+// arr2_len은 배열 arr2의 길이입니다.
+int solution(int arr1[], size_t arr1_len, int arr2[], size_t arr2_len) {
+    int sum1 = 0;
+    int sum2 = 0;
+    
+    for(int i = 0; i < (arr1_len > arr2_len ? arr1_len : arr2_len); i++){
+        if(arr1_len != arr2_len)
+            return arr1_len > arr2_len ? 1 : -1;
+        sum1 += arr1[i];
+        sum2 += arr2[i];
+    }
+    
+    if(sum1 == sum2)
+        return 0;
+            
+    return sum1 > sum2 ? 1 : -1;
+}
