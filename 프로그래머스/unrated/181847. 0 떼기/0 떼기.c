9a6cb9a85371d5ee93b61a057abd00c9f5437f77
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-18 18:32:37 +0900

    [unrated] Title: 0 떼기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.c
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.c
@@ -5,20 +5,21 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* n_str) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-
-    int num = 0;
-    int len = 0;
+    int count = 0;
     
-    num = atoi(n_str);
-    while(num){
-        num /= 10;
-        len++;
+    for(int i = 0; i < strlen(n_str); i++){
+        if(n_str[i] == '0')
+            count++;
+        else
+            break;
     }
     
-    char* answer = (char*)malloc(sizeof(char) * (len + 1));
-    memset(answer, '\0', sizeof(char) * (len + 1));
-    num = atoi(n_str);
-    sprintf(answer, "%d", num);
+    char* answer = (char*)malloc(sizeof(char) * (strlen(n_str) - count + 1));
+    memset(answer, '\0', sizeof(char) * (strlen(n_str) - count + 1));
+    
+    for(int i = 0; i < strlen(n_str) - count; i++){
+        answer[i] = n_str[i+count];
+    }
     
     return answer;
 }
