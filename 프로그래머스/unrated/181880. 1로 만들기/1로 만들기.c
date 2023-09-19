e02697f38f4affc6fdb59147d5c48d9657967222
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-19 17:42:22 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    
+    for(int i = 0; i < num_list_len; i++){
+        while(num_list[i] != 1){
+            num_list[i] /= 2;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
