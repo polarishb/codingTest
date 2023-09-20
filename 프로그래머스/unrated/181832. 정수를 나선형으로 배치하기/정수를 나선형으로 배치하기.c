ba48128aa543c163d47ef96b312aea43921f51ed
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-20 20:25:38 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.34 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.c
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.c
@@ -2,13 +2,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-enum direction {
-    up = 3,
-    down = 1,
-    left = 2,
-    right = 0
-};
-
 int** solution(int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int** answer = (int**)malloc(sizeof(int*) * n);
