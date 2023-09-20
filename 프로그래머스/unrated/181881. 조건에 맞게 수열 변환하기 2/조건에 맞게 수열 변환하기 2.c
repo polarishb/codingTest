37e1deb7d168dfed0d4adf13062aa1b30f242caa
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-20 13:45:29 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.c
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.c
@@ -5,11 +5,14 @@
 // arr_len은 배열 arr의 길이입니다.
 int solution(int arr[], size_t arr_len) {
     int answer = 0;
-    bool* check = (bool*)malloc(sizeof(bool) * arr_len + 1);
-    memset(check, true, sizeof(bool) * arr_len + 1);
+    bool check = true;
+    int temp = 0;
     
-    while(check[arr_len]){
+    while(++answer){
+        check = true;
         for(int i = 0; i < arr_len; i++){
+            temp = arr[i];
+            
             if(arr[i] < 50 && arr[i] % 2 == 1){
                 arr[i] *= 2;
                 arr[i] += 1;
@@ -17,22 +20,14 @@ int solution(int arr[], size_t arr_len) {
             else if(arr[i] >= 50 && arr[i] % 2 == 0){
                 arr[i] /= 2;
             }
-            else{
-                check[i] = false;
-            }
+            
+            if(temp != arr[i]) check = false;
+            
         }
         
-        for(int i = 0; i < arr_len; i++){
-            if(check[i]){
-                check[arr_len] = true;
-                break;
-            }
-            else
-                check[arr_len] = false;
-        }
-        answer++;
+        if(check) return answer - 1;
     }
 
     
-    return answer - 1;
+    return -1;
 }
