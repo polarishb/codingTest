40c5e5143f890076407d8b1a141c06af9aef24a1
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-22 04:04:12 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 4.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.c
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.c
@@ -8,43 +8,23 @@ int solution(int a, int b, int c, int d) {
     int count[6] = { 0, 0, 0, 0, 0, 0};
     
     for (int i = 0; i < 4; i++) {
-        switch (arr[i]) {
-        case 1:
-            count[0]++;
-            break;
-        case 2:
-            count[1]++;
-            break;
-        case 3:
-            count[2]++;
-            break;
-        case 4:
-            count[3]++;
-            break;
-        case 5:
-            count[4]++;
-            break;
-        case 6:
-            count[5]++;
-            break;
-        }
+        count[arr[i]-1]++;
     }
 
     for (int i = 0; i < 6; i++) {
-        if (count[i] == 4) {
-            return 1111 * (i + 1);
-        }
-        else if (count[i] == 3) {
-            for (int j = 0; j < 6; j++) {
-                if (count[j] == 1) return (int)pow(10 * (i + 1) + (j + 1), 2);
-            }
-        }
-        else if (count[i] == 2) {
-            for (int j = 0; j < 6; j++) {
-                if (count[j] == 2 && j != i) return ((i + 1) + (j + 1)) * abs((i + 1) - (j + 1));
-                else if (count[j] == 1) {
-                    for (int k = 0; k < 6; k++) {
-                        if (count[k] == 1 && k != j) return (j + 1) * (k + 1);
+        switch(count[i]){
+            case 4:
+                return 1111 * (i + 1);
+            case 3:
+                for (int j = 0; j < 6; j++) {
+                    if (count[j] == 1) return (int)pow(10 * (i + 1) + (j + 1), 2);
+                }
+            case 2:
+                for (int j = 0; j < 6; j++) {
+                    if (count[j] == 2 && j != i) return ((i + 1) + (j + 1)) * abs((i + 1) - (j + 1));
+                    else if (count[j] == 1) {
+                        for (int k = 0; k < 6; k++) {
+                            if (count[k] == 1 && k != j) return (j + 1) * (k + 1);
                     }
                 }
             }
