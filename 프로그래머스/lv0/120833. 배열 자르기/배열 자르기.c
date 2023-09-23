9e177a19f286af8a4d645c5275e8286afd7e1a67
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-23 16:31:52 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.c
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.c
@@ -6,10 +6,9 @@
 int* solution(int numbers[], size_t numbers_len, int num1, int num2) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * (num2 - num1 + 1));
-    int idx = 0;
     
     for(int i = num1; i <= num2; i++){
-        answer[idx++] = numbers[i];
+        answer[i - num1] = numbers[i];
     }
     return answer;
 }
