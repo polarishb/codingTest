7c3e90b42ee5c55ffc4445a9e9c11b4f98bf704e
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-27 20:23:01 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.c
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.c
@@ -4,6 +4,5 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* str1, const char* str2) {
-    if(strstr(str1, str2)) return 1;
-    else return 2;
+    return strstr(str1, str2) ? 1 : 2;
 }
