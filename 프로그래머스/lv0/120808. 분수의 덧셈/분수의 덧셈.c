e2fc9c1e99a9e7da4da21a9a396109f12c5f9c86
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-28 02:34:17 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int numer1, int denom1, int numer2, int denom2) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * 2);
+    
+    if(denom1 != denom2){
+        answer[1] = denom1 * denom2;
+        answer[0] = numer1 * denom2 + numer2 * denom1;
+    }
+    else{
+        answer[1] = denom1;
+        answer[0] = numer1 + numer2;
+    }
+    
+    int gcd = 0;
+    for(int i = 1; i <= answer[0] && i <= answer[1]; i++){
+        if(answer[0] % i == 0 && answer[1] % i == 0) gcd = i;
+    }
+    
+    answer[0] /= gcd;
+    answer[1] /= gcd;
+    
+    return answer;
+}
