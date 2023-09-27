45c9e67ab631e3da1ec72dd137ac96196f0a8735
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-28 05:03:01 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.c
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.c
@@ -2,22 +2,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int compare(void* a, void* b){
-    return *(int*)a < *(int*)b;
-}
-
 // emergency_len은 배열 emergency의 길이입니다.
 int* solution(int emergency[], size_t emergency_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * emergency_len);
-    int* arr = (int*)malloc(sizeof(int) * emergency_len);
-    memcpy(arr, emergency, sizeof(int) * emergency_len);
-    
-    qsort(arr, emergency_len, sizeof(int), compare);
     
     for(int i = 0; i < emergency_len; i++){
+        answer[i] = 1;
         for(int j = 0; j < emergency_len; j++){
-            if(arr[i] == emergency[j]) answer[j] = i + 1;
+            if(emergency[i] < emergency[j]) answer[i]++;
         }
     }
     
