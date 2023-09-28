fff813f420eb256786a8e97a28e9fd2bfe81b67a
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-29 01:53:54 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.c
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.c
@@ -5,33 +5,25 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* my_string) {
     int answer = 0;
-    bool plus = false;
-    bool minus = false;
     
     char* str = (char*)malloc(sizeof(char) * (strlen(my_string) + 1));
     memcpy(str, my_string, sizeof(char) * (strlen(my_string) + 1));
     
     char* ptr = strtok(str, " ");
-    int idx = 0;
     
     answer += atoi(ptr);
     ptr = strtok(NULL, " ");
     
     while(ptr){
         if(*ptr == '+'){
-            plus = true;
             ptr = strtok(NULL, " ");
+            answer += atoi(ptr);
         }
         else if(*ptr == '-'){
-            minus = true;
             ptr = strtok(NULL, " ");
+            answer -= atoi(ptr);
         }
         
-        if(plus) answer += atoi(ptr);
-        else if(minus) answer -= atoi(ptr);
-        
-        plus = false;
-        minus = false;
         ptr = strtok(NULL, " ");
     }
     
