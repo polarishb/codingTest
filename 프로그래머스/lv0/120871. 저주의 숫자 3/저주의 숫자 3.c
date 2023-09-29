ba487f40d7c041a650b6c7a114fe92c9369706cf
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-30 01:24:20 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.c
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.c
@@ -4,28 +4,11 @@
 
 int solution(int n) {
     int answer = 0;
-    int num = 0;
-    int idx = 0;
-    
     for(int i = 1; i <= n; i++){
         answer++;
-        num = answer;
-        idx = 0;
-        while(num /= 10){
-            idx++;
-        }
-        num = answer;
-        while(num){
-            if(!(answer % 3)){
-                answer++;
-            }
-            for(;idx+1;idx--){
-                while((answer / (int)pow(10, idx)) % 10 == 3){
-                    answer++;
-                }
-            }
-            num /= 10;
-        }        
+        while(answer % 3 == 0 || answer % 10 == 3 || answer / 10 % 10 == 3){
+            answer++;
+        }     
     }
     return answer;
 }
