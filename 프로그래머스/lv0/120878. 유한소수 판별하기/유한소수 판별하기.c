d1fb528d2a26b5ddfcb88a094f4cb1a9354fe00a
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-29 23:59:18 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.c
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.c
@@ -3,30 +3,16 @@
 #include <stdlib.h>
 
 int solution(int a, int b) {
-    int answer = 1;
-    int gcd = 1;
-    int prime = 2;
-    
-    for(int i = 2; i <= a && i <= b; i++){
-        if(a % i == 0 && b % i == 0){
-            gcd = i;
-        }
+    int answer = 2;
+
+    while(!(b % 5)){
+        b /= 5;
+    }
+    while(!(b % 2)){
+        b /= 2;
     }
     
-    b /= gcd;
-    
-    while(b > 1){
-        if(b % prime == 0){
-            if(prime != 2 && prime != 5){
-                answer = 2;
-                break;
-            }
-            b /= prime;
-        }
-        else{
-            prime++;
-        }
-    }
+    if(!(a % b)) answer = 1;
     
     return answer;
 }
