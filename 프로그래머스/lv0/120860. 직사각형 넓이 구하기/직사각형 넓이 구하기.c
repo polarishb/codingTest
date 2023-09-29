7b461b6c2ead234ef0d2ee5b332d8de5b55cdf1d
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-29 19:28:12 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.c
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.c
@@ -5,18 +5,12 @@
 // dots_rows는 2차원 배열 dots의 행 길이, dots_cols는 2차원 배열 dots의 열 길이입니다.
 int solution(int** dots, size_t dots_rows, size_t dots_cols) {
     int answer = 0;
-    int x = 0;
-    int y = 0;
     
-    for(int i = 1; i < dots_rows; i++){
-        if(dots[0][0] == dots[i][0]){
-            y = abs(dots[0][1] - dots[i][1]);
-        }
-        if(dots[0][1] == dots[i][1]){
-            x = abs(dots[0][0] - dots[i][0]);
+    for(int i = 0; i < dots_rows; i++){
+        if(dots[0][0] != dots[i][0] && dots[0][1] != dots[i][1]){
+            answer = abs(dots[0][0] - dots[i][0]) * abs(dots[0][1] - dots[i][1]);
         }
     }
     
-    answer = x * y;
     return answer;
 }
