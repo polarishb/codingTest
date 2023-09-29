4e528ca08d1ec3b0487e2ac7927a2ebcc7c5209e
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-30 01:58:30 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.c
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.c
@@ -5,22 +5,11 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* A, const char* B) {
     int answer = 0;
-    bool check = false;
-    int* arr = (int*)malloc(sizeof(int) * strlen(A));
     
     for(int i = 0; i < strlen(A); i++){
-        for(int j = 0; j < strlen(A); j++){
-            if(A[j] == B[(i + j) % strlen(A)]){
-                check = true;
-                continue;
-            }
-            else{
-                check = false;
-                break;
-            }
+        if(!strncmp(A, &B[i], strlen(A) - i) && !strncmp(&A[strlen(A) - i], B, i)){
+            return i;
         }
-        if(!check) answer++;
-        else return answer;
     }
     answer = -1;
     
