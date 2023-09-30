11c30dd0b95eb4763b6a51f62223434ce18dcedc
WOOJUN KIM <polaris.hb03@gmail.com>
2023-09-30 13:45:21 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.c
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.c
@@ -6,22 +6,10 @@ int* solution(int num, int total) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * num);
     memset(answer, 0, sizeof(int) * num);
-    int idx = num / 2;
+    int idx = (num - 1) / 2;
     
     for(int i = 0; i < num; i++){
-        answer[i] = total / num;
-    }
-    
-    if(num % 2){
-        for(int i = idx; i > 0; i--){
-            answer[num / 2 + i] += i;
-            answer[num / 2 - i] -= i;
-        }
-    }
-    else{
-        for(int i = 0; i < idx * 2; i++){
-            answer[i] = (total / num) - idx + 1 + i;
-        }
+        answer[i] = (total / num) - idx + i;
     }
     
     
